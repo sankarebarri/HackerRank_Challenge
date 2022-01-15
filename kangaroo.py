a9432ce9698f3caf3fd4907cfdbc32966caf1164
sankarebarri <sankarebarri_dev@yahoo.com>
2022-01-15 22:43:28 +0000

    kangaroo.py

---
+++ kangaroo.py
@@ -0,0 +1,9 @@
+def kangaroo(x1, v1, x2, v2):
+    '''
+    int x1, int v1: starting position and jump distance for kangaroo 1
+    int x2, int v2: starting position and jump distance for kangaroo 2
+    
+    Returns: string: either YES or NO
+
+    '''
+    pass
