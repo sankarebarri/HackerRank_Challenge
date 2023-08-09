e99bc40a0c33ec4a81ffcdb5a2824181065e0b8e
sankarebarri <sankarebarri_dev@yahoo.com>
2023-08-09 12:30:10 +0000

    day_5

---
+++ 30_days_code_challenge/day_5/loops.py
@@ -0,0 +1,8 @@
+"""
+Given an integer, n, print its first 10 multiples. Each multiple n x i (where 1 <= i <= 10) should be printed on a new line 
+in the form: n x i = result.
+"""
+
+n = int(input().strip())
+for i in range(1, 11):
+    print(f"{n} x {i} = {n * i}")
